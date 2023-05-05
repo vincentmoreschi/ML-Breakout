0649cfe79a5bd9f563c01db7e7cd39c8c52eb27f
Kai <kaichang92@gmail.com>
2023-05-05 14:13:26 -0700

    Fix clamping for variable resolutions

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
