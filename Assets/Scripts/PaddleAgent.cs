0f5d188aa0a77d0a9d0a8372a0e936c258bfe2ac
Vincent Moreschi <vmzcon@gmail.com>
2023-05-14 17:16:36 -0400

    Basic paddle agent working/models under results

--- Assets/Scripts/PaddleAgent.cs
+++ Assets/Scripts/PaddleAgent.cs
@@ -8,40 +8,46 @@ using Unity.MLAgents.Actuators;
 public class PaddleAgent : Agent
 {
     // Start is called before the first frame update
-    private int bricks; 
-    private Ball ball;
+    public int bricks; 
+    public GameObject ball;
+    
+    private BallManager ballManager;
+
     void Start()
     {
-       BallManager.Instance.CreateBall(ball);
+       ball = GameObject.Find("Ball Red(Clone)");
        bricks = LevelManager.Instance.RemainingBricks;
     }
 
     public override void OnEpisodeBegin(){
         // GameManager.Instance.gameStarted = true;
+        ball = GameObject.Find("Ball Red(Clone)");
+        bricks = LevelManager.Instance.RemainingBricks;
         
-        if(ball.transform.localPosition.y <= -4.8)
-            BallManager.Instance.ResetBall();
+        if(ball.transform.position.y < -5)
+            Debug.Log("dead");
+            
+            ball.transform.position = Vector3.zero;
+            
     }
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(ball.transform.localPosition);
-        sensor.AddObservation(this.transform.localPosition);
+        ball = GameObject.Find("Ball Red(Clone)");
+        sensor.AddObservation(ball.transform.position);
+        sensor.AddObservation(gameObject.transform.position);
 
 
     }
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        Vector3 controlSignal = Vector3.zero;
-
-        controlSignal.x = actionBuffers.ContinuousActions[0];
-
-        Vector3 direction = new Vector3(controlSignal.x, 0, 0);
-        this.transform.Translate(10  * controlSignal.x * direction);
+        Vector3 controlSignal = new Vector3(actionBuffers.ContinuousActions[0], 0, 0);
+       
+        gameObject.transform.Translate(10  * controlSignal);
 
         if(LevelManager.Instance.RemainingBricks < bricks){
             SetReward(1.0f);
         }
-        if (ball.transform.localPosition.y <= 4.8)
+        if (ball.transform.position.y < -4.5)
         {
             EndEpisode();
         }
@@ -49,7 +55,9 @@ public class PaddleAgent : Agent
     }
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+
         var continuousActionsOut = actionsOut.ContinuousActions;
         continuousActionsOut[0] = Input.GetAxis("Horizontal");
     }
+    
 }
