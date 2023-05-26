f86039b429fa6e9af7a01e301b921a0b8860a8e3
Kai <kaichang92@gmail.com>
2023-05-26 10:30:08 -0700

    Disable human input interfering with AI paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    public Player player;
+
     public float speed;
     public float horizontalBounceMultiplier;  // Affects how much the ball bounces left or right during collisions
 
@@ -13,25 +15,32 @@ public class Paddle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = transform.parent.GetComponent<Player>();
+
         _paddlePositionInitial = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Mouse X") != 0)  // If the mouse has moved along the X axis
+        // Only move the human player paddle.
+        // The AI player moves their paddle via Agent files.
+        if (player == GameManager.Instance.players[0])
         {
-            // Update paddle X position based on mouse X position
-            Vector2 position;
-            position.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-            position.y = _paddlePositionInitial.y;
-            transform.position = position;
-        }
-        else
-        {
-            // Update paddle X position based on keyboard horizontal axis input and speed
-            Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
-            transform.Translate(speed * Time.deltaTime * direction);
+            if (Input.GetAxisRaw("Mouse X") != 0)  // If the mouse has moved along the X axis
+            {
+                // Update paddle X position based on mouse X position
+                Vector2 position;
+                position.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+                position.y = _paddlePositionInitial.y;
+                transform.position = position;
+            }
+            else
+            {
+                // Update paddle X position based on keyboard horizontal axis input and speed
+                Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+                transform.Translate(speed * Time.deltaTime * direction);
+            }
         }
     }
 
