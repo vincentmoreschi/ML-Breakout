4f98f9247740c6d578bab79763ccf5c1de0f34f2
Jonathan Luong <jojo2049@gmail.com>
2023-06-04 22:02:49 -0500

    reverting PaddleAgentJL.cs to earlier version

--- Assets/Scripts/PaddleAgentJL.cs
+++ Assets/Scripts/PaddleAgentJL.cs
@@ -70,13 +70,18 @@ public class PaddleAgentJL : Agent
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
+         // Debug.Log(actions.ContinuousActions[0]);
+
+        if (player.lives < currentLives) {
+          SetReward(-1f);
+          currentLives = player.lives;
+        }
 
         if (!player.gameStarted) {
             player.paddle.ResetPosition();
             player.gameStarted = true;
             BallManager.Instance.ShootBall(player);
         }
-
 
         int move = actions.DiscreteActions[0];
         float moveSpeed = 10f;
@@ -91,6 +96,66 @@ public class PaddleAgentJL : Agent
             case 2:
                 transform.localPosition += new Vector3(moveX,0,0) * Time.deltaTime * moveSpeed;
                 break;
+        }
+
+        SetReward(-0.001f);
+
+
+        if (ball != null && (ball.transform.localPosition.y > 5.0f || ball.transform.position.y < -10f)) {
+            Debug.Log("reset higher than wall");
+            BallManager.Instance.ResetBall(player);
+            player.paddle.ResetPosition();
+            // EndEpisode();
+        }
+
+         if (player.RemainingBricks < bricks) {
+            for (int i=0;i<bricks-(player.RemainingBricks);i++) {
+                // Debug.Log("Brick break reward");
+                SetReward(.5f);
+            }
+            bricks = player.RemainingBricks;
+
+        }
+
+        if (currentLevel<player.currentLevel) {
+            SetReward(0.75f);
+            currentLevel = player.currentLevel;
+        }
+        if (player.currentLevel == 11) {
+            SetReward(1f);
+            player.victoryScreen.SetActive(false);
+            player.gameStarted = false;
+            BallManager.Instance.CreateBall(player, BallManager.Instance.ballRedPrefab);
+            EndEpisode();
+        }
+        if (player.gameOverScreen.activeSelf) {
+            SetReward(-1f);
+            player.gameOverScreen.SetActive(false);
+            player.gameStarted = false;
+            BallManager.Instance.CreateBall(player, BallManager.Instance.ballRedPrefab);
+            EndEpisode();
+
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+      if (other.collider.tag.Equals("Ball")) {
+        Debug.Log("ball hits paddle");
+        SetReward(0.75f);
+      }
+    }
+
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxisRaw("Horizontal");
+
+        if (!player.gameStarted) {
+            player.paddle.ResetPosition();
+            player.gameStarted = true;
+            BallManager.Instance.ShootBall(player);
         }
 
     }
